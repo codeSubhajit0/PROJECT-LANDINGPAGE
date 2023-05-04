b77b68edb880d00b52018f71861d4b35c9d63428
Subhajit Karmakar <subhajitk731@gmail.com>
2023-05-05 04:35:43 +0530

    Added  hamburger menue

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let Input = document.querySelector('input');
 Input.addEventListener('click',function (){
     window.open("https://codesubhajit0.github.io/Create-Account-/Login.html" , '_self');
 })
+const menue = document.querySelector('.hamburger_menue');
+
+menue.addEventListener('click', function () {
+    document.querySelector('.hamburger_menue').classList.toggle('pos')
+    document.querySelector('.line1').classList.toggle('line1h');
+    document.querySelector('.line3').classList.toggle('line3h');
+    document.querySelector('.line2').classList.toggle('line2h');
+    document.querySelector('.menue').classList.toggle('menue_vis');
+})
