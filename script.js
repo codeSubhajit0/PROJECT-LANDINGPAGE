dff2a0e4eadedc113b27daecc725cc6c9ffecfed
Subhajit Karmakar <subhajitk731@gmail.com>
2023-05-04 22:37:57 +0530

    Changed some style and added a js file

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict'
+let Input = document.querySelector('input');
+Input.addEventListener('click',function (){
+    window.open("https://codesubhajit0.github.io/Create-Account-/Login.html" , '_self');
+})
