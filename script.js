730832c71a9ec3be04ea5dcf65aaf0a586052df0
Subhajit Karmakar <subhajitk731@gmail.com>
2023-05-06 22:06:03 +0530

    made the menue invisible

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 let Input = document.querySelector('input');
-Input.addEventListener('click',function (){
-    window.open("https://codesubhajit0.github.io/Create-Account-/Login.html" , '_self');
+Input.addEventListener('click', function () {
+    window.open("https://codesubhajit0.github.io/Create-Account-/Login.html", '_self');
 })
 const menue = document.querySelector('.hamburger_menue');
 
