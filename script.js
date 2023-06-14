36fc9c78529f1471d7e29103256cd00c5c6266a1
Subhajit <subhajitk790@gmail.com>
2023-06-14 17:31:47 +0530

    CSS: Compleated the footer and fixed the time in loding page

--- script.js
+++ script.js
@@ -75,5 +75,5 @@ window.onload = function(){
     setTimeout(() => {
         document.querySelector(".loder_l").classList.add("loder_dis");
         document.querySelector('main').classList.add('main_vis');
-    }, 5900);
+    }, 3000);
 }
