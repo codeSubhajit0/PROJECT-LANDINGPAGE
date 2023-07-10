07237d8e15a731e8960717cf0cd3b3d1b06fa4e2
Subhajit <subhajitk790@gmail.com>
2023-07-11 00:56:46 +0530

    CSS: Added left border to the current contact and chat section

--- Chat Interface/app.js
+++ Chat Interface/app.js
@@ -1,6 +1,11 @@
 'use strict'
-const Send = document.querySelector('.send_button');
+const emoji = document.querySelector('.search_emoji_container');
+const Send = document.querySelector('.search_img_container');
+
+emoji.addEventListener('click', function(e) {
+    console.log('Emoji');
+});
 
 Send.addEventListener('click', function(e) {
-    console.log('Hello');
+    console.log('Send');
 });
